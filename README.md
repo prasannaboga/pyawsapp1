3e655522b53cb8a45164261b1b1d9f1d5c94aa3d
prasannaboga <prasannaboga@gmail.com>
2019-09-28 18:41:20 +0530

    Docker file for celery

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This python flask application for aws
 
 ```commandline
 docker build -f docker/web/Dockerfile -t pyawsapp1 .
+docker build -f docker/celery/Dockerfile -t pyawsapp1_celery .
 docker build -f docker/redis/Dockerfile -t ecs-redis .
 
 
@@ -12,6 +13,6 @@ docker build -f docker/redis/Dockerfile -t ecs-redis .
 $(aws ecr get-login --no-include-email --region us-east-1 --profile prasanna_awsadmin)
 
 ```commandline
-celery -A celery_tasks.celery worker -l INFO -E --autoscale=5,1 -Q apple,ball  
+celery -A celery_tasks.celery worker -l INFO -E --autoscale=5,1 -Q apple,ball,cat 
 ```
 
--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -1,6 +1,10 @@
 import time
+
+from celery import group
+from celery import chain
 from .base import celery
 from celery.utils.log import get_task_logger
+
 
 logger = get_task_logger(__name__)
 
@@ -35,3 +39,29 @@ def scheduled_task(self, **kwargs):
     time_stamp = kwargs.get('time_stamp')
     logger.info('This scheduled at {}')
     return [time_stamp]
+
+
+@celery.task(bind=True, queue="cat")
+def parent_task(self, **kwargs):
+    items_count = kwargs.get('items_count', 5)
+    logger.info('items_count = '.format(items_count))
+    l = ()
+    for i in range(1, items_count+1):
+        ct = child_task.s(i)
+        l = l + (ct,)
+    res = chain(l)()
+    return res
+
+
+@celery.task(bind=True, queue="cat")
+def child_task(self, item):
+    if not item:
+        raise Exception('No item found')
+    logger.info('item = {}'.format(item))
+    return [item]
+
+
+@celery.task(bind=True, queue="cat")
+def parent_final_task(self, **kwargs):
+
+    return ['i']
